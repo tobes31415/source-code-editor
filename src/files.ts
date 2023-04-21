fed821210d0e6a56b10e799b31032a18730599ec
Jake Tober <jaket@emids.com>
2023-04-21 18:10:20 -0400

    Don't throw an error if the root folder doesn't exist when listing files

--- src/files.ts
+++ src/files.ts
@@ -285,6 +285,10 @@ export function* RecursiveFileSearch(
   filePath: string,
   options?: RFSOptions
 ): Generator<FSNodeDescription> {
+  if (!fs.existsSync(filePath)) {
+    return;
+  }
+
   const {
     filter,
     emitFolders,
